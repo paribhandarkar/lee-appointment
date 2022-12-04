94231b401475ae5f45802041a154abbb3d9ba04c
paribhandarkar <76446574+paribhandarkar@users.noreply.github.com>
2022-12-04 16:29:24 +0530

    Add files via upload

---
+++ s.js
@@ -0,0 +1,7 @@
+// function take(){
+//     var inp1 = document.getElementById("name").value;
+//     var inp2 = document.getElementById("email").value;
+//     var inp3 = document.getElementById("contact-number").value;
+//     var x = inp1;
+// }
+
